493a15eeae8aa23fb8fc9cd7c6cf0dcafd417a3b
Mark McKelvy <65346059+mckelvym@users.noreply.github.com>
2025-10-21 07:56:28 -0400

    Always pull image for version

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,6 +2,7 @@
 
 version=1.0.1
 echo "docker run registry.hub.docker.com/mckelvym/raleigh-events-rss-generator:${version}"
+docker pull registry.hub.docker.com/mckelvym/raleigh-events-rss-generator:$version
 docker run --rm --name=visit-raleigh-harvester \
   -v $(pwd)/../../visitraleigh.events.rss:/data \
   registry.hub.docker.com/mckelvym/raleigh-events-rss-generator:$version /data/events.xml >> harvest.log
