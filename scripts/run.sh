3e2c479a68bc1a497bdeca37eb656998a2290dd8
Mark McKelvy <65346059+mckelvym@users.noreply.github.com>
2025-12-25 09:14:13 -0500

    Use image visitraleigh.events.harvester instead of raleigh-events-rss-generator

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,9 +2,9 @@
 
 # Source version from version.sh
 source "$(dirname "$0")/version.sh"
-echo "docker run registry.hub.docker.com/mckelvym/raleigh-events-rss-generator:${VERSION}"
-docker pull registry.hub.docker.com/mckelvym/raleigh-events-rss-generator:$VERSION
+echo "docker run registry.hub.docker.com/mckelvym/visitraleigh.events.harvester:${VERSION}"
+docker pull registry.hub.docker.com/mckelvym/visitraleigh.events.harvester:$VERSION
 docker run --rm --name=visit-raleigh-harvester \
   -v $(pwd)/../logs:/logs \
   -v $(pwd)/../../visitraleigh.events.rss:/data \
-  registry.hub.docker.com/mckelvym/raleigh-events-rss-generator:$VERSION /data/events.xml
+  registry.hub.docker.com/mckelvym/visitraleigh.events.harvester:$VERSION /data/events.xml
